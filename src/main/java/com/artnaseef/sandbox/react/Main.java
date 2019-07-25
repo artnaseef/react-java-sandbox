c661443709354188e0d209db53e87e82f2b0a875
artnaseef <art@artnaseef.com>
2019-07-24 20:48:30 -0700

    Add Spring Context and convert application to an example of "Hello World from Spring Context".

--- src/main/java/com/artnaseef/sandbox/react/Main.java
+++ src/main/java/com/artnaseef/sandbox/react/Main.java
@@ -1,10 +1,19 @@
 package com.artnaseef.sandbox.react;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by art on 7/24/19.
  */
 public class Main {
+    private ClassPathXmlApplicationContext applicationContext;
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Main mainInstance = new Main();
+        mainInstance.instanceMain(args);
+    }
+
+    public void instanceMain(String[] args) {
+        applicationContext = new ClassPathXmlApplicationContext("spring-context/application-context.xml");
     }
 }
