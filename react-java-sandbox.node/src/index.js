ff7b6df8d2d543fe388370bc3c8c5c51273245ab
artnaseef <art@artnaseef.com>
2019-08-01 20:22:26 -0700

    Change spacing to match project 4-space indentation.

--- react-java-sandbox.node/src/index.js
+++ react-java-sandbox.node/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class HelloWorld extends React.Component {
-  render() {
-    return (
-      <span>Hello World from React built with Node! (v2.0)</span>
-    );
-  }
+    render() {
+        return (
+            <span>Hello World from React built with Node! (v2.0)</span>
+        );
+    }
 }
 
 ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById('root')
+    <HelloWorld />,
+    document.getElementById('root')
 );
