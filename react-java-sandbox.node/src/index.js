bdec9636a59f3b471de8d838da8a84a4715aad62
artnaseef <art@artnaseef.com>
2019-08-01 21:08:48 -0700

    Use state in the react component.  Add fetch() call to which updates the rendered component with the API response.

--- react-java-sandbox.node/src/index.js
+++ react-java-sandbox.node/src/index.js
@@ -2,9 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class HelloWorld extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {serverText: ""};
+    }
+
+    receivedServerText(text) {
+        console.log(text)
+        this.setState({ serverText: text });
+    }
+
     render() {
+        fetch('/api/hello')
+            .then(function (response) {
+                      return response.text();
+                  })
+            .then((responseText) => {
+                      this.receivedServerText(responseText);
+                  }
+        );
+
         return (
-            <span>Hello World from React built with Node! (v2.0)</span>
+            <div>
+                <span>Hello World from React built with Node! (v2.0)</span>
+                <br/>
+                <span><b>server-text:</b> {this.state.serverText}</span>
+            </div>
         );
     }
 }
